0331d6d138d07187fde8e98613eae9b4b7318754
H43RO <h2is1234@naver.com>
2021-06-05 00:22:21 +0900

    같은 추론 결과에 대해서는 TTS 실행 안 하도록 수정

--- app/src/main/java/com/just_graduate/smartcane/network/NetworkHelper.kt
+++ app/src/main/java/com/just_graduate/smartcane/network/NetworkHelper.kt
@@ -2,17 +2,17 @@ package com.just_graduate.smartcane.network
 
 import android.util.Log
 import com.google.gson.GsonBuilder
+import com.just_graduate.smartcane.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 
 import retrofit2.converter.gson.GsonConverterFactory
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 object NetworkHelper {
-    private const val serverBaseUrl = "http://3.36.226.184:80"
+    private const val serverBaseUrl = BuildConfig.BASE_URL
 
     var token: String = ""
 
